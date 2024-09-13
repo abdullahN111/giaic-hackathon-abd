3c85026ad2be9ef4c293debb40892d8b7594089d
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-09-13 21:50:44 +0500

    Update script.js

--- milestone 4/script.js
+++ milestone 4/script.js
@@ -15,18 +15,25 @@ toggleButton.addEventListener("click", function () {
 //____________________________________________________________________________
 //listing elements for generating resume
 (_a = document.querySelector("#resume-form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (event) {
+    var _a;
     event.preventDefault();
     //type assertion to elements
+    var profilePic = document.querySelector('#profile-picture');
     var nameElement = document.querySelector("#name");
     var emailElement = document.querySelector("#email");
     var phoneElement = document.querySelector("#phone");
+    var addressElement = document.querySelector("#address");
+    var dobElement = document.querySelector("#dob");
     var educationElement = document.querySelector("#education");
     var experienceElement = document.querySelector("#experience");
     var expertiseElement = document.querySelector("#expertise");
     var hobbiesElement = document.querySelector("#hobbies");
-    if (nameElement &&
+    if (profilePic &&
+        nameElement &&
         emailElement &&
         phoneElement &&
+        addressElement &&
+        dobElement &&
         educationElement &&
         experienceElement &&
         expertiseElement &&
@@ -34,19 +41,26 @@ toggleButton.addEventListener("click", function () {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
+        var address = addressElement.value;
+        var dob = dobElement.value;
         var education = educationElement.value;
         var experience = experienceElement.value;
         var expertise = expertiseElement.value;
         var hobbies = hobbiesElement.value;
+        // picture element
+        var profilePicFile = (_a = profilePic.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePicUrl = profilePicFile ? URL.createObjectURL(profilePicFile) : "";
         //Generating output
-        var createResume = "\n  <div class=\"resume-created\">\n      <h1> Your Resume </h2>\n    <h2> <span id=\"edit-name\" class=\"editable\">".concat(name_1, "</span></h2>\n    <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\"> ").concat(email, "</span></p>\n    <p><strong>Phone:</strong><span id=\"edit-phone\" class=\"editable\"> ").concat(phone, "</span></p>\n    \n    <fieldset>\n      <legend>Education</legend>\n      <p id=\"edit-education\" class=\"editable\">").concat(education, "</p>\n    </fieldset>\n  \n    <fieldset>\n      <legend>Experience</legend>\n      <p id=\"edit-experience\" class=\"editable\">").concat(experience, "</p>\n    </fieldset>\n  \n    <fieldset>\n      <legend>Expertise</legend>\n      <p id=\"edit-expertise\" class=\"editable\">").concat(expertise, "</p>\n    </fieldset>\n  \n    ").concat(hobbies
-            ? "<fieldset>\n      <legend>Hobbies</legend>\n      <p id=\"edit-hobbies\" class=\"editable\">".concat(hobbies, "</p>\n    </fieldset>")
-            : "", "\n  \n  </div>\n  ");
+        var createResume = "\n    <div class=\"resume-created\">\n        <h1> Your Resume </h2>\n        ".concat(profilePicUrl ? "<img src=\"".concat(profilePicUrl, "\" alt=\"profile picture\" class=\"profile-picture\">") : "", "\n      <h2><span id=\"edit-name\" class=\"editable\">").concat(name_1, "</span></h2>\n      <p> <strong>DOB:</strong><span id=\"edit-dob\" class=\"editable\"> ").concat(dob, "</span></p>\n      <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\"> ").concat(email, "</span></p>\n      <p><strong>Phone:</strong><span id=\"edit-phone\" class=\"editable\"> ").concat(phone, "</span></p>\n      <p><strong>Address:</strong><span id=\"edit-address\" class=\"editable\"> ").concat(address, "</span></p>\n      \n      <fieldset>\n        <legend>Education</legend>\n        <p><span id=\"edit-education\" class=\"editable\"> ").concat(education, "</span></p>\n      </fieldset>\n    \n      <fieldset>\n        <legend>Experience</legend>\n        <p><span id=\"edit-experience\" class=\"editable\"> ").concat(experience, "</span></p>\n      </fieldset>\n    \n      <fieldset>\n        <legend>Expertise</legend>\n        <p><span id=\"edit-expertise\" class=\"editable\"> ").concat(expertise, "</span></p>\n      </fieldset>\n    \n      ").concat(hobbies
+            ? "<fieldset>\n        <legend>Hobbies</legend>\n        <p><span id=\"edit-hobbies\" class=\"editable\"> ".concat(hobbies, "</span></p>\n      </fieldset>")
+            : "", "\n    \n    </div>\n    ");
         // Insert the generated resume into the DOM
         var resumeElement = document.querySelector(".resume-created");
         if (resumeElement) {
             resumeElement.innerHTML = createResume;
-            makeEditable();
+        }
+        else {
+            console.error("the elements are missing");
         }
     }
     else {
